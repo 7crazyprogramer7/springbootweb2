e6558000c382bfee282aff3f022ecfd277eab842
7crazyprogramer7 <3150687874@qq.com>
2019-09-25 17:59:35 +0800

    root commit springboot-web

---
+++ src/main/java/cn/cq/springboot/config/AppConfig.java
@@ -0,0 +1,21 @@
+package cn.cq.springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        WebMvcConfigurer configurer = new WebMvcConfigurer() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("login");
+            }
+        };
+        return configurer;
+    }
+}
---
+++ src/main/java/cn/cq/springboot/controller/HelloController.java
@@ -0,0 +1,23 @@
+package cn.cq.springboot.controller;
+
+import cn.cq.springboot.pojo.User;
+import com.google.gson.Gson;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping(value = "/hello",method = RequestMethod.POST,consumes = "application/json")
+    public String hello(@RequestBody User user) {
+        System.out.println(new Gson().toJson(user));
+        return new Gson().toJson(user);
+    }
+    @PostMapping(value = "/Login")
+    public String login(String username,String pwd){
+
+        //使用重定向定位资源
+        return "redirect:page/main.html";
+    }
+}
