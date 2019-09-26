2b387699542c2f87a7b876a29c242ad18ea3a19d
7crazyprogramer7 <3150687874@qq.com>
2019-09-26 21:49:12 +0800

    delete gti from controller

--- src/main/java/cn/cq/springboot/controller/HelloController.java
+++ src/main/java/cn/cq/springboot/controller/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
     @ResponseBody
     @RequestMapping(value = "/hello",method = RequestMethod.POST,consumes = "application/json")
     public String hello(@RequestBody User user) {
-         gti
+
         return new Gson().toJson(user);
     }
     @PostMapping(value = "/Login")
